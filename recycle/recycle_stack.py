2be90ea39c6199f4bb540052ff00759f12274595
Harry Hummel <hhhummel@gmail.com>
2023-12-16 21:47:07 -0500

    Dynamo and state machine

--- recycle/recycle_stack.py
+++ recycle/recycle_stack.py
@@ -1,8 +1,10 @@
 import os
 from aws_cdk import (
-    # Duration,
     Stack,
     aws_lambda as lambda_,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
+    aws_dynamodb as dynamodb
 )
 from constructs import Construct
 
@@ -12,14 +14,28 @@ class RecycleStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        dynamodb.Table(self, "RecycleUserTable",
+            partition_key=dynamodb.Attribute(
+                name="id",
+                type=dynamodb.AttributeType.STRING
+            ),
+            table_name="RecycleUserTable",
+            deletion_protection=True,
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "RecycleQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-        fn = lambda_.Function(self, "GetTrashZoneLowerMerion",
+        lm_zone_function = lambda_.Function(
+            self, 
+            "GetTrashZoneLowerMerion",
             code=lambda_.Code.from_asset(os.path.join(os.getcwd(), "src")),
             handler="get_trash_zone_lower_merion.lambda_handler",
             runtime=lambda_.Runtime.PYTHON_3_9
+        )
+
+        state_machine = sfn.StateMachine(
+            self, 
+            "ConfigureUserStateMachine",
+            definition=tasks.LambdaInvoke(
+                self, "ZoneTask",
+                lambda_function=lm_zone_function
+            ).next(sfn.Succeed(self, "GreetedWorld"))
         )
