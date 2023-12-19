01b3d827d9477801c562b28c739a2c8e16e94cc4
Harry Hummel <hhhummel@gmail.com>
2023-12-18 22:11:49 -0500

    Add cognito and api gateway

--- recycle/recycle_stack.py
+++ recycle/recycle_stack.py
@@ -4,9 +4,12 @@ from aws_cdk import (
     aws_lambda as lambda_,
     aws_stepfunctions as sfn,
     aws_stepfunctions_tasks as tasks,
-    aws_dynamodb as dynamodb
+    aws_dynamodb as dynamodb,
+    aws_cognito as cognito,
+    aws_apigateway as gateway,
 )
 from constructs import Construct
+
 
 class RecycleStack(Stack):
 
@@ -23,19 +26,33 @@ class RecycleStack(Stack):
             deletion_protection=True,
         )
 
+        pool = cognito.UserPool(self, "Pool")
+        pool.add_client(
+            "app-client",
+            o_auth=cognito.OAuthSettings(
+                flows=cognito.OAuthFlows(
+                    authorization_code_grant=True
+                ),
+                scopes=[cognito.OAuthScope.OPENID],
+                callback_urls=["https://sashette.com/welcome"],
+                logout_urls=["https://sashette.com/signin"]
+            )
+        )
+
+        api = gateway.RestApi(self, "recycle-api")
+        api.root.add_method("ANY")
+        users = api.root.add_resource("users")
+        users.add_method("GET")
+        users.add_method("POST")
+        user = users.add_resource("{user_id}")
+        user.add_method("GET")
+        user.add_method("DELETE")
+        user.add_method("PATCH")
+
         lm_zone_function = lambda_.Function(
             self, 
             "GetTrashZoneLowerMerion",
             code=lambda_.Code.from_asset(os.path.join(os.getcwd(), "src")),
             handler="get_trash_zone_lower_merion.lambda_handler",
             runtime=lambda_.Runtime.PYTHON_3_9
-        )
-
-        state_machine = sfn.StateMachine(
-            self, 
-            "ConfigureUserStateMachine",
-            definition=tasks.LambdaInvoke(
-                self, "ZoneTask",
-                lambda_function=lm_zone_function
-            ).next(sfn.Succeed(self, "GreetedWorld"))
         )
